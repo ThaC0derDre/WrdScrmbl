8c94ff0b7cfcbec44dc3ed61c67385b2eb0bfe6b
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-17 13:28:47 -0700

    Initial Commit

---
+++ WrdScrmbl/WrdScrmblApp.swift
@@ -0,0 +1,17 @@
+//
+//  WrdScrmblApp.swift
+//  WrdScrmbl
+//
+//  Created by Andres Gutierrez on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WrdScrmblApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
