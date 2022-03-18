3694dd9c33668af1a865a445b97cd0a7ec81319b
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-17 17:09:03 -0700

    Set up basic terms. function to save words entered by user

--- WrdScrmbl/ContentView.swift
+++ WrdScrmbl/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWord = [String]()
+    @State private var newWord  = ""
+    @State private var rootWord = ""
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section{
+                    TextField("Enter a word", text: $newWord)
+                }
+                Section{
+                    ForEach(usedWord, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
+    }
+
+    func addNewWord() {
+        let answer  = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count < 0 else { return }
+        
+        usedWord.insert(answer, at: 0)
+        
+        newWord = ""
     }
 }
 
